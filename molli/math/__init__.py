8691ce3f0a6e7d5868f8b4a04bd76939decbb05b
Alexander Shved <51724651+esalx@users.noreply.github.com>
2023-03-20 13:55:00 -0500

    more math

--- molli/math/__init__.py
+++ molli/math/__init__.py
@@ -1,2 +1,7 @@
-from .rotation import rotation_matrix_from_vectors, rotation_matrix_from_axis
+from .rotation import (
+    rotation_matrix_from_vectors,
+    rotation_matrix_from_axis,
+    rotate_2dvec_outa_plane,
+)
 from .distance import _optimize_rotation
+from .plane import mean_plane
