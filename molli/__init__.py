230349ddffec8ba3f996492b7916f88894cc82f4
Alexander Shved <51724651+esalx@users.noreply.github.com>
2023-04-10 01:06:53 -0500

    1.0.0a7 version increment

--- molli/__init__.py
+++ molli/__init__.py
@@ -20,7 +20,7 @@ It also implements a lot of command line tools (run `molli --HELP` or `molli lis
 
 """
 
-__version__ = "1.0.0a6"
+__version__ = "1.0.0a7"
 
 # Determine whether molli C++ extensions are available
 # if not, pure python analogs should be provided
