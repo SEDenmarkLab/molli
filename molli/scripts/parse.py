efaf101b34877afabab751586d1c44f306a18bcb
esalx <shvedalx@gmail.com>
2023-05-17 18:55:55 -0500

    parse script args

--- molli/scripts/parse.py
+++ molli/scripts/parse.py
@@ -1,5 +1,5 @@
 """
-This package parses chemical files, such as .cdxml, and creates a collection.
+This package parses chemical files, such as .cdxml, and creates a collection of molecules in .mlib format.
 """
 import molli as ml
 from pprint import pprint
@@ -12,7 +12,27 @@ arg_parser = ArgumentParser(
     description=__doc__,
 )
 
-arg_parser.add_argument("files", nargs="*")
+arg_parser.add_argument("file", action="store", help="File to be parsed.")
+
+arg_parser.add_argument(
+    "-i",
+    "--input",
+    action="store",
+    metavar="<fpath>",
+    default=None,
+    help=(
+        "Override the source file format. Defaults to the file extension. Supported types: 'cdxml'"
+    ),
+)
+
+arg_parser.add_argument(
+    "-o",
+    "--output",
+    action="store",
+    metavar="<fpath>",
+    default=None,
+    help="Destination for .MLIB output",
+)
 
 
 def molli_main(args, config=None, output=None, **kwargs):
