c3a05fff81ae9e2b1aff4596d4042772bde88b72
Sara Lambert <lambert8@illinois.edu>
2023-06-23 19:45:07 -0500

    feat: improved logging from subprocess

--- ncsa-testing/main.py
+++ ncsa-testing/main.py
@@ -3,14 +3,14 @@
 import molli as ml
 import molli.visual
 import subprocess
+import threading
 import os
 
 import json
 
-# For logging
+import logging
 import sys
 import time
-import logging
 
 # This is a failsafe in case openbabel aint installed
 #import openbabel
@@ -39,16 +39,6 @@ def obsvg(mol: ml.Molecule, *, display_name: bool = False, alias: bool = True, c
 
     return conv.WriteString(obmol)
 
-# Set up logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler("debug.log"),
-        logging.StreamHandler(sys.stdout)
-    ]
-)
-
 # TODO: Read everything below from envs
 
 # INPUT ARGS: number of cores / concurrent jobs
@@ -60,23 +50,72 @@ clustering_removed_variance_columns = 0
 clustering_cutoff = 0.8
 
 # INPUT FILES: chemdraw files that the user passes in
-cores = ml.files.box_cores_test_1
-subs = ml.files.box_substituents_test_1
+cores = os.getenv('CORES_INPUT_FILE', ml.files.box_cores_test_1)
+subs = os.getenv('SUBS_INPUT_FILE', ml.files.box_substituents_test_1)
 
 # OUTPUT FILES: output directory
-out_dir = './ncsa-testing-output/'
+out_dir = os.getenv('JOB_OUTPUT_DIR', '/molli/ncsa-testing-output/')
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(f"{out_dir}/log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+# Pipe for passing subprocess output to logging
+class LogPipe(threading.Thread):
+
+    def __init__(self, level):
+        """Setup the object with a logger and a loglevel
+        and start the thread
+        """
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.level = level
+        self.fdRead, self.fdWrite = os.pipe()
+        self.pipeReader = os.fdopen(self.fdRead)
+        self.start()
+
+    def fileno(self):
+        """Return the write file descriptor of the pipe
+        """
+        return self.fdWrite
+
+    def run(self):
+        """Run the thread, logging everything.
+        """
+        for line in iter(self.pipeReader.readline, ''):
+            logging.log(self.level, line.strip('\n'))
+
+        self.pipeReader.close()
+
+    def close(self):
+        """Close the write end of the pipe.
+        """
+        os.close(self.fdWrite)
 
 def parse_chemdraw():
     logging.info("=== Parsing ChemDraw Files ===")
-    # parse the files
-    subprocess.run(['molli', 'parse', '--hadd', f'{cores}', '-o', f'{out_dir}BOX_cores_new_env.mlib', "--overwrite"])
-    subprocess.run(['molli', 'parse', '--hadd', f'{subs}', '-o', f'{out_dir}BOX_subs_new_env.mlib', "--overwrite"])
 
-    m_core = ml.MoleculeLibrary(f'{out_dir}BOX_cores_new_env.mlib')
+    # parse the files
+
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(['molli', 'parse', '--hadd', f'{cores}', '-o', f'{out_dir}/BOX_cores_new_env.mlib', "--overwrite"], stdout=logpipe, stderr=logpipe) as s:
+        logpipe.close()
+
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(['molli', 'parse', '--hadd', f'{subs}', '-o', f'{out_dir}/BOX_subs_new_env.mlib', "--overwrite"], stdout=logpipe, stderr=logpipe) as s:
+        logpipe.close()
+
+    m_core = ml.MoleculeLibrary(f'{out_dir}/BOX_cores_new_env.mlib')
     logging.debug(len(m_core))
     # TODO enforce maximum number of cores?
 
-    m_subs = ml.MoleculeLibrary(f'{out_dir}BOX_subs_new_env.mlib')
+    m_subs = ml.MoleculeLibrary(f'{out_dir}/BOX_subs_new_env.mlib')
     logging.debug(len(m_subs))
     # TODO enforce maximum number of substituents?
 
@@ -84,51 +123,57 @@ def parse_chemdraw():
 # Combine step (takes ~2 minutes)
 def combinatorial_expansion():
     logging.info("=== Performing Combinatorial Expansion ===")
-    subprocess.run(
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(
         [
             'molli',
             'combine',
-            f'{out_dir}BOX_cores_new_env.mlib',
+            f'{out_dir}/BOX_cores_new_env.mlib',
             '-s',
-            f'{out_dir}BOX_subs_new_env.mlib',
+            f'{out_dir}/BOX_subs_new_env.mlib',
             '-j',
-            f'{thread_concurrency}',
-            '-o',
-            f'{out_dir}test_combine_new_env.mlib',
-            '-a',
-            'A1',
-            '--obopt',
+            f'{thread_concurrency}', 
+            '-o', 
+            f'{out_dir}/test_combine_new_env.mlib', 
+            '-a', 
+            'A1', 
+            '--obopt', 
             'uff',
             '-m',
             'same',
             "--overwrite"
-        ]
-    )
+        ],
+        stdout=logpipe,
+        stderr=logpipe
+    ) as s:
+        logpipe.close()
 
 
-    combined = ml.MoleculeLibrary(f'{out_dir}test_combine_new_env.mlib')
+    combined = ml.MoleculeLibrary(f'{out_dir}/test_combine_new_env.mlib')
     logging.debug(len(combined))
-    with open(f'{out_dir}test_combine_new_env_library.json', 'w') as f:
+    with open(f'{out_dir}/test_combine_new_env_library.json', 'w') as f:
         json.dump({item.name: {'mol2': item.dumps_mol2(), 'svg': obsvg(item)} for item in combined}, f)
 
 # Conformers step (takes ~4 minutes)
 def generate_conformers():
     logging.info("=== Generating Conformers ===")
-    subprocess.run(['molli',
-                    'conformers',
-                    f'{out_dir}test_combine_new_env.mlib',
-                    '-n',
-                    f'{job_concurrency}',
-                    '-o',
-                    f'{out_dir}test_conformers_new_env.mlib',
-                    '-t',
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(['molli', 
+                    'conformers', 
+                    f'{out_dir}/test_combine_new_env.mlib', 
+                    '-n', 
+                    f'{job_concurrency}', 
+                    '-o', 
+                    f'{out_dir}/test_conformers_new_env.mlib', 
+                    '-t', 
                     '-j', ### !!!!!! Number of jobs. Please scale down if host system has fewer cores. defaults to os.cpu_count()//2  !!!!! ###
                     f'{thread_concurrency}',
                     "--overwrite"
-                    ])
+                    ], stdout=logpipe, stderr=logpipe) as s:
+         logpipe.close()
 
 
-    clib = ml.ConformerLibrary(f'{out_dir}test_conformers_new_env.mlib')
+    clib = ml.ConformerLibrary(f'{out_dir}/test_conformers_new_env.mlib')
     logging.debug(len(clib))
 
     i = 0
@@ -150,63 +195,73 @@ def generate_conformers():
 def aso_descriptor():
     logging.info("=== Generating ASO Descriptor ===")
     # first we make a grid for calculating aso
-    subprocess.run(['molli',
-                    'grid',
-                    '--mlib',
-                    f'{out_dir}test_conformers_new_env.mlib',
-                    '-o',
-                    f'{out_dir}grid_new_env.npy'
-                    ])
+    
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(['molli', 
+                    'grid', 
+                    '--mlib', 
+                    f'{out_dir}/test_conformers_new_env.mlib', 
+                    '-o', 
+                    f'{out_dir}/grid_new_env.npy'
+                    ], stdout=logpipe, stderr=logpipe) as s:
+        logpipe.close()
     # calculate aso
-    subprocess.run(['molli',
-                    'gbca',
-                    'aso',
-                    f'{out_dir}test_conformers_new_env.mlib',
-                    '-g',
-                    f'{out_dir}grid_new_env.npy',
-                    '-o',
-                    f'{out_dir}aso_new_env.h5'
-                    ])
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(['molli', 
+                    'gbca', 
+                    'aso', 
+                    f'{out_dir}/test_conformers_new_env.mlib', 
+                    '-g', 
+                    f'{out_dir}/grid_new_env.npy', 
+                    '-o', 
+                    f'{out_dir}/aso_new_env.h5'
+                    ], stdout=logpipe, stderr=logpipe) as s:
+        logpipe.close()
     # tqdm looks messed up
 
 
 def post_processing():
     logging.info('=== Running Post-Processing ===')
-    subprocess.run(         # check functionality for plotting and pca
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(         # check functionality for plotting and pca
          [                   # should be better way to implement post_processing stuff
              'molli',
              'cluster',
-             f'{out_dir}aso_new_env.h5',
+             f'{out_dir}/aso_new_env.h5',
              '-m',
              'tsne',
              '-o',
-             f'{out_dir}new_env_data3_tsne',
-             '-v', # variance threshold before doing clustering
-             f'{clustering_removed_variance_columns}', # remove 0 variance columns
-             '-c', # correlation cutoff before clustering
-             f'{clustering_cutoff}', # 0.8 by default
-         ]
-     )
-    subprocess.run(         # check functionality for plotting and pca
+             f'{out_dir}/new_env_data3_tsne',
+         ], stdout=logpipe, stderr=logpipe
+     ) as s:
+        logpipe.close()
+
+    logpipe = LogPipe(logging.INFO)
+    with subprocess.Popen(         # check functionality for plotting and pca
          [                   # should be better way to implement post_processing stuff
              'molli',
              'cluster',
-             f'{out_dir}aso_new_env.h5',
+             f'{out_dir}/aso_new_env.h5',
              '-m',
              'pca',
              '-o',
-             f'{out_dir}new_env_data3_pca',
+             f'{out_dir}/new_env_data3_pca',
              '-v', # variance threshold before doing clustering
              f'{clustering_removed_variance_columns}', # remove 0 variance columns
              '-c', # correlation cutoff before clustering
              f'{clustering_cutoff}', # 0.8 by default
-         ]
-     )
+         ], stdout=logpipe, stderr=logpipe
+     ) as s:
+         logpipe.close()
 
 
 def main():
     logging.info('=== Starting Job ===')
     start_time = time.time()
+
+    logging.info(f"INPUT cores: {cores}")
+    logging.info(f"INPUT substituents: {subs}")
+    logging.info(f"OUTPUT folder: {out_dir}")
 
     # create output directory if it doesn't exist
     if not os.path.exists(out_dir):
