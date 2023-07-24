7baa46167a7948b747db65e0944b978c2d04af84
blakeo2 <blakeo2@illinois.edu>
2023-07-24 14:04:12 -0500

    Update moleucle.py to match NCSA +Fixed formatting +Added "coords" attribute to Molecule + Added "coords" attribute to super().__init__ + Added serialize function to Molecule + Fixed Formatting

--- molli/chem/molecule.py
+++ molli/chem/molecule.py
@@ -7,7 +7,7 @@ from io import StringIO, BytesIO
 import re
 from warnings import warn
 
-from molli.chem import AtomLike
+from molli.chem import Atom, AtomLike
 
 from . import (
     Element,
@@ -36,6 +36,7 @@ class Molecule(Structure):
         charge: int = None,
         mult: int = None,
         name: str = None,
+        coords: ArrayLike = None,
         atomic_charges: ArrayLike = ...,
         **kwds,
     ):
@@ -45,7 +46,7 @@ class Molecule(Structure):
         # if isinstance(other, Molecule | Structure):
         #     ...
         super().__init__(
-            other, n_atoms=n_atoms, name=name, charge=charge, mult=mult, **kwds
+            other, n_atoms=n_atoms, name=name, charge=charge, mult=mult, coords=coords, **kwds
         )
         self.atomic_charges = atomic_charges
 
@@ -70,7 +71,10 @@ class Molecule(Structure):
             if _pc.shape == (self.n_atoms,):
                 self._atomic_charges = _pc
             else:
-                raise ValueError("Inappropriate shape of atomic charge array")
+                raise ValueError(
+                    f"Inappropriate shape of atomic charge array. Received: {_pc.shape}, expected:"
+                    f" {(self.n_atoms,)}"
+                )
 
     def dump_mol2(self, stream: StringIO = None):
         if stream is None:
@@ -78,13 +82,14 @@ class Molecule(Structure):
             
         stream.write(f"# Produced with molli package\n")
         stream.write(
-            f"@<TRIPOS>MOLECULE\n{self.name}\n{self.n_atoms} {self.n_bonds} 0 0 0\nSMALL\nUSER_CHARGES\n\n"
+            f"@<TRIPOS>MOLECULE\n{self.name}\n{self.n_atoms} {self.n_bonds} 0 0"
+            " 0\nSMALL\nUSER_CHARGES\n\n"
         )
 
         stream.write("@<TRIPOS>ATOM\n")
         for i, a in enumerate(self.atoms):
             x, y, z = self.coords[i]
-            c = 0.0 #Currently needs to be updated to be inherited within the structure or even individual atoms
+            c = 0.0  # Currently needs to be updated to be inherited within the structure or even individual atoms
             label = a.label or a.element.symbol
             atype = a.get_mol2_type() or a.element.symbol
             stream.write(
@@ -94,9 +99,9 @@ class Molecule(Structure):
         stream.write("@<TRIPOS>BOND\n")
         for i, b in enumerate(self.bonds):
             a1, a2 = self.atoms.index(b.a1), self.atoms.index(b.a2)
-            btype = b.get_mol2_type()            
+            btype = b.get_mol2_type()
             stream.write(f"{i+1:>6} {a1+1:>6} {a2+1:>6} {btype:>10}\n")
-        
+
     def dumps_mol2(self) -> str:
         """
         This returns a mol2 file as a string
@@ -105,10 +110,66 @@ class Molecule(Structure):
         self.dump_mol2(stream)        
         return stream.getvalue()
     
+    def add_atom(self, a: Atom, coord: ArrayLike, charge: float = None):
+        super().add_atom(a, coord)
+        self._atomic_charges = np.append(self._atomic_charges, [charge], axis=0)
+
     def del_atom(self, _a: AtomLike):
         _i = self.get_atom_index(_a)
         super().del_atom(_a)
         self._atomic_charges = np.delete(self._atomic_charges, _i, axis=0)
-        
 
+    def serialize(self):
+        atom_id_map = {a: i for i, a in enumerate(self.atoms)}
+
+        atoms = [a.as_tuple() for a in self.atoms]
+        bonds = [b.as_tuple(atom_id_map=atom_id_map) for b in self.bonds]
+
+        return (
+            self.name,
+            self.n_atoms,
+            atoms,
+            bonds,
+            self.charge,
+            self.mult,
+            self.coords.astype(">f4").tobytes(),
+            self.atomic_charges.astype(">f4").tobytes(),
+        )
+
+    @classmethod
+    def deserialize(cls: type[Molecule], struct: tuple):
+        (
+            _name,
+            _na,
+            _atoms,
+            _bonds,
+            _charge,
+            _mult,
+            _coords,
+            _atomic_charges,
+        ) = struct
+
+        coords = np.frombuffer(_coords, dtype=">f4").reshape((_na, 3))
+        atomic_charges = np.frombuffer(_atomic_charges, dtype=">f4")
+
+        atoms = []
+        for i, a in enumerate(_atoms):
+            atoms.append(Atom(*a))
+
+        res = cls(
+            atoms,
+            n_atoms=_na,
+            name=_name,
+            charge=_charge,
+            mult=_mult,
+            coords=coords,
+            atomic_charges=atomic_charges,
+        )
+
+        for i, b in enumerate(_bonds):
+            a1, a2, *b_attr = b
+            res.append_bond(Bond(atoms[a1], atoms[a2], *b_attr))
+
+        return res
+    
 StructureLike = Molecule | Structure | Substructure
