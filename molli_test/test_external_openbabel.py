b4eb8adb1b0cdcaedb2bf453e589ad5b6d85e6f8
KazuriCode <elenaburlova93@gmail.com>
2023-11-29 21:56:59 -0600

    black formatting for openbabel unit-tests

--- molli_test/test_external_openbabel.py
+++ molli_test/test_external_openbabel.py
@@ -10,32 +10,39 @@ except:
     _OPENBABEL_INSTALLED = False
 else:
     from molli.external import openbabel as _ob
+
     _OPENBABEL_INSTALLED = True
 
-class OpenbabelTC(ut.TestCase):
 
-    @ut.skipUnless(_OPENBABEL_INSTALLED, "Openbabel is not installed in current environment")
+class OpenbabelTC(ut.TestCase):
+    @ut.skipUnless(
+        _OPENBABEL_INSTALLED, "Openbabel is not installed in current environment"
+    )
     def test_ob_mol2(self):
         with ml.files.dendrobine_mol2.open() as f:
             mlmol = ml.Molecule.load_mol2(f)
-        
+
         mol2_block = _ob.to_mol2_w_ob(mlmol)
 
         new_mlmol = next(ml.Structure.yield_from_mol2(mol2_block))
 
-        self.assertEqual(len(mlmol.atoms),len(new_mlmol.atoms))
-        self.assertEqual(len(mlmol.bonds),len(new_mlmol.bonds))
-    
-    @ut.skipUnless(_OPENBABEL_INSTALLED, "Openbabel is not installed in current environment")
+        self.assertEqual(len(mlmol.atoms), len(new_mlmol.atoms))
+        self.assertEqual(len(mlmol.bonds), len(new_mlmol.bonds))
+
+    @ut.skipUnless(
+        _OPENBABEL_INSTALLED, "Openbabel is not installed in current environment"
+    )
     def test_ob_opt(self):
         with ml.files.dendrobine_mol2.open() as f:
             mlmol = next(ml.Molecule.yield_from_mol2(f))
 
         old_coords = deepcopy(mlmol.coords)
 
-        opt_mlmol = _ob.obabel_optimize(mlmol,ff='UFF')
+        opt_mlmol = _ob.obabel_optimize(mlmol, ff="UFF")
 
         opt_coords = opt_mlmol.coords
         self.assertTupleEqual(old_coords.shape, opt_coords.shape)
 
-        np.testing.assert_raises(AssertionError,np.testing.assert_array_equal, old_coords,opt_coords)
+        np.testing.assert_raises(
+            AssertionError, np.testing.assert_array_equal, old_coords, opt_coords
+        )
