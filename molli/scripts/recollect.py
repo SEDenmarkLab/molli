54447358ed2ad6e383313794e197701a42f72930
Blake Ocampo <blakeo2@illinois.edu>
2023-12-06 18:01:51 -0600

    Prototype of Recollect Function

--- molli/scripts/recollect.py
+++ molli/scripts/recollect.py
@@ -8,6 +8,12 @@ from tqdm import tqdm
 import zipfile
 import msgpack
 from pathlib import Path
+from zipfile import ZipFile, is_zipfile
+import numpy as np
+from ..storage import Collection
+from molli.external import openbabel as mob
+from functools import partial
+import sys
 
 arg_parser = ArgumentParser(
     "molli recollect",
@@ -15,21 +21,52 @@ arg_parser = ArgumentParser(
 )
 
 arg_parser.add_argument(
-    "input",
-    metavar="XML_FILE",
+    "-it",
+    "--input_type",
+    # metavar="<MLIB_FILE, CLIB_File, ZIP_FILE, OLD_XML_FILE, UKV_FILE, DIRECTORY>",
+    choices=["zip","mlib","mli","clib",'cli','dir','directory'],
+    action="store",
+    type=str.lower,
+    help="This is the input type, including <mlib>, <.clib>, <.zip>, <.xml>, <.ukv>, or directory (<dir>)",
+)
+
+arg_parser.add_argument(
+    "-i",
+    "--input",
+    metavar="<PATH>",
+    action="store",
+    type=Path,
+    help="This is the input path",
+)
+
+arg_parser.add_argument(
+    "-ot",
+    "--output_type",
+    choices=["mlib","clib","dir"],
     action="store",
     type=str,
-    help="Old style xml collection to be converted",
+    default=...,
+    help="New style collection, either with or without conformers",
 )
 
 arg_parser.add_argument(
     "-o",
     "--output",
-    metavar="MLI_FILE",
+    metavar="<PATH>",
     action="store",
-    type=str,
+    type=Path,
     default=...,
-    help="New style collection to be made",
+    help="This is the output path",
+)
+
+arg_parser.add_argument(
+    "-p",
+    "--parser",
+    choices=["molli","obabel"],
+    action="store",
+    type=str.lower,
+    default='molli',
+    help="This indicates the type of parser, will default to molli for xyz and mol2, all other files will default to openbabel",
 )
 
 arg_parser.add_argument(
@@ -51,43 +88,94 @@ arg_parser.add_argument(
     help="Increase the amount of output",
 )
 
+#Currently do not have a skip function in this
+# arg_parser.add_argument(
+#     "-s",
+#     "--skip",
+#     action="store_true",
+#     default=False,
+#     help="This option enables skipping malformed files within old collections. Warnings will be printed.",
+# )
+
 arg_parser.add_argument(
-    "-s",
-    "--skip",
-    action="store_true",
-    default=False,
-    help="This option enables skipping malformed files within old collections. Warnings will be printed.",
+    "-ext",
+    "--extension",
+    action="store",
+    default='mol2',
+    type=str,
+    help="This option is required if reading from a <zip> or directory to indicate the File Type being searched for (<mol2>, <xyz>, etc.)",
 )
 
-mol_conf = arg_parser.add_mutually_exclusive_group()
+def recollect(collect:Collection, lib, charge, mult, clib, mlib, dir):
+    '''
+    Overarching function that recollects for different types of collections:
 
-mol_conf.add_argument(
-    "-mol",
-    "--molecules",
-    action="store_true",
-    default=False,
-    help="This will parse the geometry into a ml.MoleculeLibrary"
-)
+    This is currently built assuming either "loads_all_xyz" or "loads_all_mol2" was run.
+    This can be changed, but I figured we would stick to a list for now and we could change it after
 
-mol_conf.add_argument(
-    "-conf",
-    "--conformers",
-    action="store_true",
-    default=True,
-    help="This will parse the geometry into a ml.ConformerEnsemble"
-)
+    '''
+    with collect.reading(), lib.writing():
+        #Currently needs an implementation to show how far it is using tqdm
+        for name in collect:
+            res_list = collect[name]
+            test_mol = res_list[0]
+            # print(test_mol)
+            if isinstance(test_mol, ml.Molecule):
+
+                if clib:
+                    all_coords = np.array([m.coords for m in res_list])
+                    ens = ml.ConformerEnsemble(test_mol, coords=all_coords, n_conformers=len(res_list))
+                    # ens = ml.ConformerEnsemble(res_list)
+                    ens.charge = charge
+                    ens.mult = mult
+                    lib[test_mol.name] = ml.ConformerEnsemble(res_list)
+
+                elif mlib or dir:
+                    for m in res_list:
+                        m.charge = charge
+                        m.mult = mult
+                        lib[m.name] = m             
+
+            else:
+                raise NotImplementedError(f'Currently unable to work directly from {type(test_mol)}.')
+
+    sys.exit(f'Finished recollecting')
+
+def recollect_legacy(zf: ZipFile, lib, charge, mult, mlib):
+    with lib.writing():
+        #Currently needs an implementation to show how far it is using tqdm
+        for xml in zf.namelist():
+            if xml != '__molli__':
+                try:
+                    res = ml.chem.ensemble_from_molli_old_xml(zf.open(xml), mol_lib=mlib)
+                except SyntaxError:
+                    print(f'File {xml} in source collection cannot be read')
+                res.charge = charge
+                res.mult = mult
+                print(res.dumps_mol2())
+                print('hello')
+                lib[res.name] = res
+    
+    sys.exit(f'Finished recollecting legacy file')
 
 def molli_main(args,  **kwargs):
     parsed = arg_parser.parse_args(args)
 
     inp = Path(parsed.input)
+    it = parsed.input_type
 
-    if parsed.output is Ellipsis:
-        out = inp.with_suffix(".mli")
-        with ml.aux.ForeColor("yellow"):
-            print(f"Defaulting to {out} as the output destination.")
-    else:
-        out = Path(parsed.output)
+    if not it:
+        it = inp.suffix
+
+    out = Path(parsed.output)
+    ot = parsed.output_type
+
+    # if parsed.output is Ellipsis:
+    #     out = inp.with_suffix(".mli")
+    #     with ml.aux.ForeColor("yellow"):
+    #         print(f"Defaulting to {out} as the output destination.")
+    # else:
+    #     out = Path(parsed.output)
 
     charge, mult = parsed.charge_mult
 
@@ -97,50 +185,112 @@ def molli_main(args,  **kwargs):
         print(" - Input  ", inp.absolute())
         print(" - Output ", out.absolute())
 
-    if parsed.skip:
-        with ml.aux.ForeColor("yellow"):
-            print(f"Enabled skipping malformed files.")
+    # if parsed.skip:
+    #     with ml.aux.ForeColor("yellow"):
+    #         print(f"Enabled skipping malformed files.")
 
-    try:
-        zf = zipfile.ZipFile(inp, "r")
-    except:
-        print("This does not appear to be a valid zip file")
-        exit(2)
+    mlib = False
+    clib = False
+    dir = False
+            
+    encoding_method = {}
+    decoding_method = {}
 
-    if "__molli__" not in zf.namelist():
-        print("This does not appear to be a valid collection")
-        exit(3)
+    match parsed.parser:
+        case 'molli':
+            decoding_method[f'xyz'] = ml.Molecule.loads_all_xyz
+            decoding_method[f'mol2'] = ml.Molecule.loads_all_mol2
+            encoding_method[f'xyz'] = (lambda x: ml.Molecule.dumps_xyz(x).encode())
+            encoding_method[f'mol2'] = (lambda x: ml.Molecule.dumps_mol2(x).encode())
+        case 'obabel':
+            decoding_method[parsed.extension] = partial(mob.loads_obmol, input_ext=parsed.extension, connect_perceive=False, cls = ml.Molecule)
+            encoding_method[parsed.extension] = partial(mob.dumps_obmol, ftype=parsed.extension)
+        case _:
+            raise NotImplementedError(f'{parsed.parser} not an available parser')
 
-    lib_type = ml.chem.ConformerLibrary
+    match ot:
+        case 'mlib':
+            lib = ml.chem.MoleculeLibrary(parsed.output, readonly=False, overwrite=True)
+            mlib = True
+        case 'clib':
+            lib = ml.chem.ConformerLibrary(parsed.output, readonly=False, overwrite=True)
+            clib = True
+        case 'dir':
+            lib = ml.storage.Collection[dict](
+                parsed.output,
+                ml.storage.DirCollectionBackend,
+                ext=f".{parsed.extension}",
+                value_encoder=encoding_method[parsed.extension],
+                # value_decoder=decoding_method[parsed.extension],
+                readonly=False,
+                overwrite=True
+            )
+            dir = True
+            # raise NotImplementedError('"dir" not impelemented in current iteration')
+        case _:
+            raise NotImplementedError(f'{ot} not implemented in current iteration')
 
-    if parsed.molecules:
-        lib_type = ml.chem.MoleculeLibrary
+    match it:
+        case 'zip'|'ZIP_FILE':
+            # print('zip')
+            if not is_zipfile(inp):
+                raise ValueError(f'{inp} is not a valid zipfile!')
+            else:
+                with ZipFile(inp, mode='r') as zf:
+                    #Searches the keys for a file unique to molli 0.2
+                    if '__molli__' in zf.NameToInfo:
+                        recollect_legacy(zf,lib,charge, mult, mlib)
 
-    with (
-        ml.aux.catch_interrupt(),
-        lib_type.new(out) as lib
-        ):
+                    suffixes = {Path(x).suffix for x in zf.namelist()}
+                assert len(suffixes) == 1, f'There are not uniform file types in this ZipFile: {suffixes}'
 
-        tnm = 0
-        tnc = 0
-        for f in tqdm(zf.filelist, f"Reading data from {inp.name}", dynamic_ncols=True):
-            if f.filename != "__molli__":
-                try:
-                    ens = ml.chem.ensemble_from_molli_old_xml(zf.open(f), mol_lib=parsed.molecules)
-                except SyntaxError:
-                    tqdm.write(f"File {f} in source collection cannot be read.")
-                    if parsed.skip:
-                        continue
-                ens.charge = charge
-                ens.mult = mult
+                # if not parsed.extension:
+                #     #Finds extension without period
+                #     parsed.extension = suffixes.pop()[1:]
+                
+                zip_col = ml.storage.Collection[dict](
+                    inp,
+                    ml.storage.ZipCollectionBackend,
+                    ext=f'.{parsed.extension}',
+                    value_decoder=decoding_method[parsed.extension],
+                    readonly=True,
+                    overwrite=False
+                )
+                #Read Zip and Write New Library
+                recollect(zip_col, lib, charge=charge, mult=mult, clib=clib, mlib=mlib, dir=dir)
 
-                tnm += 1
-                if parsed.molecules:
-                    tnc = 1
-                else:
-                    tnc += ens.n_conformers
+        case 'mlib'|'mli':
+            # print('mlib')
+            mlib = ml.MoleculeLibrary(inp)
+            assert parsed.molecules, f'This should be recollecting into an ml.MoleculeLibrary, This value must be True!'
+            
+            with mlib.reading(), lib.writing():
+                for name in mlib:
+                    m = mlib[name]
+                    m.charge = charge
+                    m.mult = mult
+                    lib[name] = m
+        
+        case 'clib'|'cli':
+            # print('clib')
+            clib = ml.ConformerLibrary(inp)
+            assert parsed.conformers, f'This should be recollecting into an ml.ConformerLibrary, This value must be True!'
+            
+            with clib.reading(), lib.writing():
+                for name in clib:
+                    ens = clib[name]
+                    ens.charge = charge
+                    ens.mult = mult
+                    lib[name] = ens
 
-                lib.append(ens.name, ens)
+        case 'dir'|'Directory'|'directory':
+            # print('dir')
+            dir_collect = ml.storage.Collection[dict](
+                inp,
+                ml.storage.DirCollectionBackend,
+                ext=f".{parsed.extension}",
+                value_decoder=decoding_method[parsed.extension],
+                readonly=True,
+            )
 
-    with ml.aux.ForeColor("green"):
-        print("Success")
+            recollect(dir_collect, lib, charge=charge, mult=mult, clib=clib, mlib=mlib, dir=dir)
