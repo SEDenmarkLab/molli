6acb8c114745b2a09d5f6de6a8a5e7992e43cf25
esalx <shvedalx@gmail.com>
2023-12-06 19:50:47 -0600

    runner named commands

--- molli/pipeline/runner.py
+++ molli/pipeline/runner.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from argparse import ArgumentParser
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from subprocess import run
+from subprocess import run, DEVNULL
 import os
 import shlex
 import sys
@@ -25,7 +25,7 @@ arg_parser.add_argument(
     "-o",
     "--output",
     action="store",
-    required=True
+    required=True,
     type=Path,
     metavar="<output_file>",
 )
@@ -61,16 +61,19 @@ def run_local():
     scratch_dir = Path(job.scratch_dir)
     scratch_dir.mkdir(parents=True, exist_ok=True)
 
+    _cwd_original = os.getcwd()
+
     with TemporaryDirectory(dir=scratch_dir, prefix=job.jid + "__") as td:
         cwd = Path(td)
+        os.chdir(cwd)
 
         if isinstance(job.files, dict):
             for fn, fc in job.files.items():
                 if isinstance(fc, str):
-                    with open(cwd / fn, "wt") as f:
+                    with open(fn, "wt") as f:
                         f.write(fc)
                 else:
-                    with open(cwd / fn, "wb") as f:
+                    with open(fn, "wb") as f:
                         f.write(fc)
 
         environ = os.environ.copy()
@@ -79,18 +82,33 @@ def run_local():
             environ |= job.envars
 
         fail = None
+        names = []
 
-        for i, cmd in enumerate(job.commands):
-            with (
-                open(cwd / f"__stdout.{i}.txt", "wt") as stdout,
-                open(cwd / f"__stderr.{i}.txt", "wt") as stderr,
-            ):
+        for i, (cmd, name) in enumerate(job.commands):
+            if name is not None:
+                # Named commands get their stdout and stderr recorded
+                names.append(name)
+
+                with (
+                    open(f"{name}.out", "wt") as stdout,
+                    open(f"{name}.err", "wt") as stderr,
+                ):
+                    proc = run(
+                        shlex.split(cmd),
+                        cwd=cwd,
+                        env=environ,
+                        stderr=stderr,
+                        stdout=stdout,
+                        encoding="utf8",
+                    )
+
+            else:
                 proc = run(
                     shlex.split(cmd),
                     cwd=cwd,
                     env=environ,
-                    stderr=stderr,
-                    stdout=stdout,
+                    stderr=DEVNULL,
+                    stdout=DEVNULL,
                     encoding="utf8",
                 )
 
@@ -98,30 +116,28 @@ def run_local():
                 fail = i
                 break
 
-        stdouts = []
-        stderrs = []
+        stdouts = {}
+        stderrs = {}
 
-        for i in range(len(job.commands) if fail is None else fail + 1):
+        for name in names:
             with (
-                open(cwd / f"__stdout.{i}.txt", "rt") as stdout,
-                open(cwd / f"__stderr.{i}.txt", "rt") as stderr,
+                open(cwd / f"{name}.out", "rt") as stdout,
+                open(cwd / f"{name}.err", "rt") as stderr,
             ):
-                stdouts.append(stdout.read())
-                stderrs.append(stderr.read())
-
-        print(fail, job.return_files)
+                stdouts[name] = stdout.read()
+                stderrs[name] = stderr.read()
 
         if proc.returncode == 0:
             retfiles = {
-                f: (cwd / f).read_bytes()
-                for f in job.return_files
-                if (cwd / f).is_file()
+                f: f.read_bytes() for f in map(Path, job.return_files) if f.is_file()
             }
+
+    os.chdir(_cwd_original)
 
     out = ml.pipeline.JobOutput(
         exitcode=proc.returncode,
-        stdout=stdouts,
-        stderr=stderrs,
+        stdouts=stdouts,
+        stderrs=stderrs,
         files=retfiles,
     )
 
