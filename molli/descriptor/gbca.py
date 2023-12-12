1d8bd5850da64238c937fef802d722f72cc820e1
esalx <shvedalx@gmail.com>
2023-12-11 22:52:54 -0600

    gbca indicator

--- molli/descriptor/gbca.py
+++ molli/descriptor/gbca.py
@@ -4,6 +4,7 @@ This is a foundational file
 """
 import numpy as np
 from numpy.typing import ArrayLike
+import molli as ml
 
 
 def rectangular_grid(
@@ -36,19 +37,55 @@ def rectangular_grid(
     return np.column_stack((xx.ravel(), yy.ravel(), zz.ravel()))
 
 
-# class Grid:
-#     def __init__(self, grid: ArrayLike = None, dtype: str = "float32"):
-#         self._dtype = dtype
-#         self.grid = np.array(grid, dtype=self._dtype)
+def indicator(
+    grid: np.ndarray,
+    struct_or_ens: ml.ConformerEnsemble | ml.CartesianGeometry,
+    dtype: np.dtype = np.int64,
+    max_dist: float | str = "vdw_radius",
+):
+    """Returns an indicator array
+    in the shape of (n_conformers, n_gridpoints) for ensembles
+    and in the shape of (n_gridpoints,) for StructureLikes
+    where the number corresponds to the *atom index* (or -1 if the closest atom is farther than max_dist)
 
-#     @property
-#     def grid(self):
-#         return self._grid
+    Uses KDTree data structure to increase the rate of computations significantly
+    """
+    from scipy.spatial import KDTree
 
-#     @grid.setter
-#     def grid(self, other: ArrayLike | None):
-#         self._grid = np.array(other, self._dtype)
+    # This is to handle very specific cases
+    if isinstance(max_dist, str):
+        max_dist = np.array([getattr(a, max_dist) for a in struct_or_ens.atoms])
 
-#     @property
-#     def n_gridpoints(self):
-#         return self._grid.shape[0]
+    if isinstance(struct_or_ens, ml.ConformerEnsemble):
+        result = np.empty((struct_or_ens.n_conformers, grid.shape[0]), dtype=dtype)
+
+        for i, c in enumerate(struct_or_ens):
+            ckd = KDTree(c.coords)
+            dd, ii = ckd.query(grid, distance_upper_bound=max_dist, k=1)
+            result[i] = np.where(dd <= max_dist, ii, -1)
+
+    elif isinstance(struct_or_ens, ml.CartesianGeometry):
+        dd, ii = KDTree(struct_or_ens.coords).query(grid, distance_upper_bound=2.0, k=1)
+        result = np.asarray(np.where(dd <= 2.0, ii, -1), dtype=dtype)
+
+    return result
+
+
+def prune(
+    grid: np.ndarray,
+    struct_or_ens: ml.ConformerEnsemble | ml.CartesianGeometry,
+    eps: float = 0.5,
+    max_dist: float = 2.0,
+):
+    """
+    This function prunes the grid to return only the points closer than (max_dist + eps) from an ensemble or molecule.
+    This is a very fast (!) implementation that utilizes a KDTree structure for fast queries
+    """
+    from scipy.spatial import KDTree
+
+    if isinstance(struct_or_ens, ml.ConformerEnsemble):
+        kdt = KDTree(np.vstack(struct_or_ens.coords))
+    else:
+        kdt = KDTree(struct_or_ens.coords)
+    dd, ii = kdt.query(grid, eps=eps, distance_upper_bound=max_dist)
+    return np.where(dd <= max_dist)[0]
