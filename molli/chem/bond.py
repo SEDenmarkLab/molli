5942ffe88584eb853818b5ad5d922bd64ac9c362
Austin Douglas <austind4@illinois.edu>
2023-12-14 18:40:23 -0600

    Unedited Changes to bond.py from Sphinx

--- molli/chem/bond.py
+++ molli/chem/bond.py
@@ -23,6 +23,22 @@ import networkx as nx
 
 
 class BondType(IntEnum):
+    """# `BondType`
+
+    Enumerates through different kinds of bonds and assignes hydrogen donor and acceptorclassifications
+
+    Args:
+
+        IntEnum (cls): inherited class for enumeration of integers
+
+    Potential Uses:
+    ```Python
+        double_bond = BondType(2) # Double
+        donor = BondType(100) # hydrogen donor
+        donor == BondType.H_Donor # True
+    ```
+    """
+
     Unknown = 0
     Single = 1
     Double = 2
@@ -44,6 +60,16 @@ class BondType(IntEnum):
 
 
 class BondStereo(IntEnum):
+    """
+    Enumerates through different classifications for bond stereochemistry
+
+    Example Usage:
+
+        >>> trans = BondStereo(10) # E
+        >>> trans == BondStereo.E # True
+        >>> trans == BondStereo.Trans # True
+    """
+
     Unknown = 0
     NotStereogenic = 1
 
@@ -80,7 +106,10 @@ MOL2_BOND_TYPE_MAP = bidict(
 
 @attrs.define(slots=True, repr=True, hash=False, eq=False, weakref_slot=True)
 class Bond:
-    """`a1` and `a2` are always assumed to be interchangeable"""
+    """
+    The class for bonds in the MOLLI package.
+    a1 and a2 are the atoms that the bond connects, and their order is assumed to be interchangeable.
+    """
 
     a1: Atom = attrs.field(repr=lambda a: a.idx or a)
     a2: Atom = attrs.field(repr=lambda a: a.idx or a)
@@ -123,11 +152,34 @@ class Bond:
     def parent(self, other):
         self._parent = other
 
-    def evolve(self, **changes):
+    def evolve(self, **changes) -> Bond:
+        """
+        Creates a new bond with the same atoms, but with the specified changes
+
+        Args:
+            **changes: the changes to make to the bond class
+
+        Returns:
+            Bond: the new bond with the specified changes
+
+        Example Usage:
+            >>> bond = Bond(a1, a2, order=2)
+            >>> bond.evolve(order=3) # Bond(a1, a2, order=3)
+        """
         return attrs.evolve(self, **changes)
 
     @property
     def order(self) -> float:
+        """
+        The order of the bond
+
+        Returns:
+            float: the order of the bond
+
+        Example Usage:
+            >>> bond = Bond(a1 = "C", a2 = "H")
+            >>> print(bond.order) # 1.0
+        """
         # if self.btype == BondType.FractionalOrder:
         #     return self._order
         # elif 0 < self.btype < 9:
@@ -153,22 +205,76 @@ class Bond:
             case _:
                 return 1.0
 
-    def as_dict(self, schema: List[str] = None):
+    def as_dict(self, schema: List[str] = None) -> dict:
+        """
+        Converts the bond to a dictionary
+
+        Args:
+            atom_id_map (dict): a dictionary mapping atom ids to atom indices
+
+        Returns:
+            dict: the bond as a dictionary
+
+        Example Usage:
+            >>> bond = Bond(a1 = "C", a2 = "H")
+            >>> bond.as_dict({0: 'C', 1: 'H'})
+            >>> print(bond) # {0: 'C', 1: 'H'}
+        """
         if schema is None:
             return attrs.asdict(self)
         else:
             return {a: getattr(self, a, None) for a in schema}
 
-    def as_tuple(self, schema: List[str] = None):
+    def as_tuple(self, schema: List[str] = None) -> tuple:
+        """
+        Converts the bond to a tuple
+
+        Args:
+            atom_id_map (dict): a tuple mapping atom ids to atom indices
+
+        Returns:
+            tuple: the bond as a tuple
+
+        Example Usage:
+            >>> bond.as_tuple({0: "C", 1:"H"})
+            >>> print(bond) # ('C', 'H')
+        """
         if schema is None:
             return attrs.astuple(self)
         else:
             return tuple(getattr(self, a, None) for a in schema)
 
-    def __contains__(self, other: Atom):
+    def __contains__(self, other: Atom) -> bool:
+        """
+        boolean check if an atom is in the bond
+
+        Args:
+            other (Atom): the atom to check
+
+        Returns:
+            bool: True if the atom is in the bond, False otherwise
+
+        Example Usage:
+            >>> bond = Bond(a1 = "C", a2 = "H")
+            >>> bond.__contains__("C") # TRUE
+        """
         return other in {self.a1, self.a2}
 
     def __eq__(self, other: Bond | set):
+        """
+        Boolean check if two bonds are equal
+
+        Args:
+            other (Bond | set): the bond to check
+
+        Returns:
+            bool: True if the bonds are equal, False otherwise
+
+        Example Usage:
+            >>> bond_1 = Bond(a1 = "C", a2 = "H")
+            >>> bond_2 = Bond(a1 = "C", a2 = "H")
+            >>> bond_1 == bond_2 # True
+        """
         # return self is other
         match other:
             case Bond():
@@ -180,6 +286,16 @@ class Bond:
                 raise ValueError(f"Cannot equate <{type(other)}: {other}>, {self}")
 
     def __repr__(self):
+        """
+        Prints the bond
+
+        Returns:
+            str: the string representation of the bond
+
+        Example Usage:
+            >>> bond = Bond(a1 = "C", a2 = "H")
+            >>> bond # Bond(C, H, order=1.0)
+        """
         return f"Bond({self.a1}, {self.a2}, order={self.order})"
 
     # This mimics the default behavior of object instances in python
@@ -263,16 +379,46 @@ class Connectivity(Promolecule):
 
     @property
     def bonds(self) -> List[Bond]:
+        """
+        List of all bonds in the molecule
+
+        Returns:
+            List[Bond]: list of all bonds in the molecule
+
+        Example Usage:
+            >>> molecule = Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.bonds) # [Bond(C, H, order=1.0), Bond(C, C, order=1.0), Bond(C, O, order=2.0), Bond(C, Cl, order=1.0)]
+        """
         return self._bonds
 
     @property
-    def n_bonds(self):
+    def n_bonds(self) -> int:
+        """
+        The total number of bonds in the molecule
+
+        Returns:
+            int: the total number of bonds in the molecule
+
+        Example Usage:
+            >>> molecule = Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.n_bonds) # 7
+        """
         return len(self._bonds)
 
-    def lookup_bond(self, a1: AtomLike, a2: AtomLike):
+    def lookup_bond(self, a1: AtomLike, a2: AtomLike) -> Bond | None:
         """
-        Returns a bond that connects the two atoms. O(N).
-        NOTE: `a1` and `a2` are always assumed to be interchangeable in this context.
+        Retrieves the bond that connects two atoms
+
+        Args:
+            a1 (AtomLike): the first atom
+            a2 (AtomLike): the second atom
+
+        Returns:
+            Bond | None: the bond that connects the two atoms, or None if the bond does not exist
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.lookup_bond("C", "H")) # Bond(C, H, order=1.0)
         """
         _a1 = self.get_atom(a1)
         _a2 = self.get_atom(a2)
@@ -288,6 +434,18 @@ class Connectivity(Promolecule):
         return b
 
     def index_bond(self, b: Bond) -> int:
+        """
+        Index of Desired Bond
+
+        Args:
+            b (Bond): the bond to find the index of
+        Returns:
+            int: the index of the bond
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.index_bond(Bond("C", "H"))) # 0
+        """
         return self._bonds.index(b)
 
     def get_bond(self, b: Bond | int) -> Bond:
@@ -305,38 +463,132 @@ class Connectivity(Promolecule):
                 raise ValueError(f"Unable to fetch a bond with {type(b)}: {b}")
 
     def append_bond(self, bond: Bond):
+        """
+        Adds a bond to the molecule
+
+        Args:
+            bond (Bond): the bond to add to the molecule
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> molecule.append_bond(Bond("C", "H"))
+            >>> print(molecule.n_bonds) # 8
+        """
         self._bonds.append(bond)
         bond.parent = self
 
     def append_bonds(self, *bonds: Bond):
+        """
+        Adds multiple bonds to the molecule
+
+        Args:
+            *bonds (Bond): the bonds to add to the molecule
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> molecule.append_bonds(Bond("C", "H"), Bond("C", "H"))
+            >>> print(molecule.n_bonds) # 9
+        """
         self._bonds.extend(bonds)
         for b in bonds:
             b.parent = self
 
     def extend_bonds(self, bonds: Iterable[Bond]):
+        """
+        Adds multiple bonds to the molecule
+
+        Args:
+            *bonds (Bond): the bonds to add to the molecule
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> molecule.append_bonds(Bond("C", "H"), Bond("C", "H"))
+            >>> print(molecule.n_bonds) # 9
+        """
         self.append_bonds(*bonds)
 
     def del_bond(self, b: Bond):
+        """
+        Deletes a bond from the molecule
+
+        Args:
+            b (Bond): the bond to delete from the molecule
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> molecule.del_bond(Bond("C", "H"))
+            >>> print(molecule.n_bonds) # 6
+        """
         self._bonds.remove(b)
 
     def del_atom(self, _a: AtomLike):
+        """
+        Deletes an atom from the molecule
+
+        Args:
+            _a (AtomLike): the atom to delete from the molecule
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> molecule.del_atom("C")
+            >>> print(molecule.get_atom("C")) # [Atom(H), Atom(O), Atom(Cl)]
+        """
         tbd = list(self.bonds_with_atom(_a))
         for b in tbd:
             self.del_bond(b)
         super().del_atom(_a)
 
     def bonds_with_atom(self, a: AtomLike) -> Generator[Bond, None, None]:
+        """
+        Each bond on an atom
+
+        Args:
+            a (AtomLike): the atom to find the bonds of
+
+        Yields:
+            Generator[Bond, None, None]: the bonds on the atom
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.bonds_with_atom("C")) # [Bond(C, H, order=1.0), Bond(C, C, order=1.0), Bond(C, O, order=2.0), Bond(C, Cl, order=1.0)]
+        """
         _a = self.get_atom(a)
         for b in self._bonds:
             if _a in b:
                 yield b
 
     def connected_atoms(self, a: AtomLike) -> Generator[Atom, None, None]:
+        """
+        Each atom connected to an atom
+
+        Args:
+            a (AtomLike): the atom to find the connected atoms of
+
+        Yields:
+            Generator[Atom, None, None]: the atoms connected to the atom
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.connected_atoms("C")) # [Atom(C), Atom(H), Atom(C), Atom(O), Atom(Cl)]
+        """
         _a = self.get_atom(a)
         for b in self.bonds_with_atom(_a):
             yield b % _a
 
     def bonded_valence(self, a: AtomLike):
+        """
+        Sum of valences of all atoms bonded to an atom
+
+        Args:
+            a (AtomLike): the atom to find the bonded valence of
+
+        Returns:
+            float: the sum of valences of all atoms bonded to the atom
+
+        Example Usage:
+            >>> molecule = ml.Connectivity('Acetyl Chloride') # CH3COCl
+            >>> print(molecule.bonded_valence("C")) # 4.0
+        """
         # TODO: rewrite using sum()
         _a_bonds = self.bonds_with_atom(a)
 
@@ -347,6 +599,15 @@ class Connectivity(Promolecule):
         return val
 
     def n_bonds_with_atom(self, a: AtomLike):
+        """
+        Total number of bonds on an atom
+
+        Args:
+            a (AtomLike): the atom to find the number of bonds of
+
+        Returns:
+            int: the total number of bonds on the atom
+        """
         return sum(1 for _ in self.connected_atoms(a))
 
     # def _bfs_single(self, q: deque, visited: set):
@@ -363,12 +624,22 @@ class Connectivity(Promolecule):
         _direction: AtomLike = None,
     ) -> Generator[Tuple[Atom, int], None, None]:
         """
-        Yields atoms and their distances from start
+        Yields atoms in breadth-first search, in traversal order.
 
-        ```python
-        for atom, distance in connectivity.yield_bfsd(a):
-            ...
-        ```
+        Distance from the start atom is also yielded.
+
+        Args:
+            start (AtomLike): the atom to start the search from
+
+        Yields:
+            Generator[Tuple[Atom, int], None, None]: the atoms in breadth-first search, in traversal order
+
+            Yields atoms and their distances from start
+
+        Example Usage:
+
+            >>> for atom, distance in connectivity.yield_bfsd(a):
+
         """
         start = self.get_atom(_start)
         visited = {start}
