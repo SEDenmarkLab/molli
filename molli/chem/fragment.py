87635e3002b9780ad1014c9d7db0994b49fdfd1c
Austin Douglas <austind4@illinois.edu>
2023-12-14 18:41:20 -0600

    Unedited Documentation Changes to fragment.py from Sphinx

--- molli/chem/fragment.py
+++ molli/chem/fragment.py
@@ -5,6 +5,17 @@ from numpy.typing import ArrayLike
 class Substituent(Structure):
     """
     Substituent is a structure with only one(!) predefined attachment point
+
+    Args:
+        other (Structure, optional): Another Structure object to initialize from.
+        n_atoms (int, optional): The number of atoms in the substituent.
+        name (str, optional): The name of the substituent.
+        coords (ArrayLike, optional): The coordinates of the substituent's atoms.
+        copy_atoms (bool, optional): Flag indicating whether to copy the atoms from `other`.
+        charge (int, optional): The charge of the substituent.
+        mult (int, optional): The multiplicity of the substituent.
+        attachment_point (AtomLike): The predefined attachment point of the substituent.
+        kwds: Additional keyword arguments to pass to the parent class.
     """
 
     def __init__(
