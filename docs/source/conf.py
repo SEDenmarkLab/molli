743fe6bf06d4551fdda15da13331ce584ab2192a
Alexander Shved <51724651+esalx@users.noreply.github.com>
2024-01-29 12:48:18 -0600

    test furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,5 +60,5 @@ latex_engine = "xelatex"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
