58ba3049e59328552e87726a1783accd6a7c8671
blakeo2 <blakeo2@illinois.edu>
2025-05-23 11:43:17 -0500

    Reference for work in creating the interface

--- molli/external/morfeus.py
+++ molli/external/morfeus.py
@@ -1,3 +1,22 @@
+# ================================================================================
+# This file is part of `molli 1.0`
+# (https://github.com/SEDenmarkLab/molli)
+#
+# Developed by Blake E. Ocampo
+#
+# S. E. Denmark Laboratory, University of Illinois, Urbana-Champaign
+# https://denmarkgroup.illinois.edu/
+#
+# Copyright 2022-2023 The Board of Trustees of the University of Illinois.
+# All Rights Reserved.
+#
+# Licensed under the terms MIT License
+# The License is included in the distribution as LICENSE file.
+# You may not use this file except in compliance with the License.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+# ================================================================================
+
 import molli as ml
 import numpy as np
 import importlib.util
